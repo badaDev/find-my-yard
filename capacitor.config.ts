6ab921f8f9bc0c91b4e25fe5e7f7c988d30cee67
nurudeen <nurudeen@zarttech.com>
2025-01-29 14:19:39 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Find My Yard',
+  webDir: 'www'
+};
+
+export default config;
